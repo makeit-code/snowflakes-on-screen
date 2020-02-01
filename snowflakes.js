471bfdf49b0772eec8391d76cd8032235e3511f5
Michał Szynklarz <github@mszynklarz.opole.pl>
2020-02-01 14:56:00 +0100

    Snowflakes generator

---
+++ snowflakes.js
@@ -0,0 +1,17 @@
+setInterval(createSnowFlake, 100);
+
+function createSnowFlake () {
+    const snow_flake = document.createElement('i');
+    snow_flake.classList.add('fas');
+    snow_flake.classList.add('fa-snowflake');
+    snow_flake.style.left = Math.random() * window.innerWidth + 'px';
+    snow_flake.style.animationDuration = Math.random() * 6 + 5 + 's';
+    snow_flake.style.opacity = Math.random();
+    snow_flake.style.fontSize = Math.random() * 10 + 10 + 'px';
+
+    document.body.appendChild(snow_flake);
+
+    setTimeout(() => {
+        snow_flake.remove();
+    }, 10000)
+}
